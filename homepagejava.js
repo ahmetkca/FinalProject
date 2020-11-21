3020e0ccac0d56ce63b9aa548dea0b43bb14f370
airwick989 <73313597+airwick989@users.noreply.github.com>
2020-11-20 20:11:05 -0500

    g

--- homepagejava.js
+++ homepagejava.js
@@ -1,5 +1,38 @@
-function slideshow(){
+var slidecount = 0;
 
-    
+function nextslide(){
+
+    slidecount++;
+
+    if(slidecount > 1){
+        slidecount = 0;
+    }
+
+    slide();
+
+}
+
+function nextslide(){
+
+    slidecount++;
+
+    if(slidecount > 1){
+        slidecount = 0;
+    }
+
+    slide();
+
+}
+
+function slide(){
+
+    if(slidecount == 0){
+        document.getElementById("0").style.display = 'block';
+        document.getElementById("1").style.display = 'none';
+    }
+    else if(slidecount == 1){
+        document.getElementById("0").style.display = 'none';
+        document.getElementById("1").style.display = 'block';
+    }
 
 }
