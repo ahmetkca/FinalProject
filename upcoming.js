0589ec9bb007389ce1c037169ad4fa855c956143
Mfrack22 <74574227+Mfrack22@users.noreply.github.com>
2020-11-21 16:21:18 -0500

    Create upcoming.js

---
+++ upcoming.js
@@ -0,0 +1,11 @@
+var Like = 0 ;
+function AddLike(){
+Like = Like + 1;
+document.getElementById('lClicks').innerHTML = Like ;
+}
+
+var disLike = 0 ;
+function AdddisLike(){
+disLike = disLike + 1;
+document.getElementById('dClicks').innerHTML = disLike ;
+}
